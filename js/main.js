d351e30afe47b38a052f3526a3dd07bd3d900f46
Nyulian122 <103065477+Nyulian122@users.noreply.github.com>
2024-02-10 10:23:46 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,49 +1,27 @@
-let tareas= [
-    {
-        estado : true,
-        id : 1,
-        texto : "Quitar todos lo tornillos de la tapa" 
-    },
-    {
-        estado : false,
-        id : 2,
-        texto : "Levantar la tapa con cuidado" 
-    },
-    {
-        estado : true,
-        id : 3,
-        texto : "Desconectar la bateria" 
-    },
-    {
-        estado : false,
-        id : 4,
-        texto : "Identificar componentes" 
-    },
-]
+import { tareas } from './todo_.js';
 
-function cargar_tareas(){
-    let cuadro_de_tareas = document.querySelector(".root")
-    tareas.forEach((cada_tarea) => {
+function cargar_tareas() {
+    let cuadro_de_tareas = document.querySelector(".root");
 
-        let div_tarea = document.createElement("div")
-        div_tarea.classList.add("div_tareas")
-        if(cada_tarea.estado){
+    tareas.forEach(cada_tarea => {
+        let div_tarea = document.createElement("div");
+        div_tarea.classList.add("div_tareas");
 
         div_tarea.innerHTML = `
-        <p class="text">${cada_tarea.texto}</p>
-        <div class="estado">[X]</div>
-        `;}else{
-
-            div_tarea.innerHTML = `
-        <p class="text">${cada_tarea.texto}</p>
-        <div class="estado">[X]</div>
+            <p class="text">${cada_tarea.texto}</p>
+            <div class="estado">${cada_tarea.estado ? '[X]' : '[_]'}</div>
         `;
-
-        }
-
+        
         cuadro_de_tareas.appendChild(div_tarea);
-
-    })
+    });
 }
 
 cargar_tareas();
+
+function cargar_botones() {
+    let caja_btn = document.querySelector(".botones");
+    
+    caja_btn.innerHTML = `<div class="btn_mas"> + </div>`;
+}
+
+cargar_botones();
