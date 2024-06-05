59957cf5b26d96270bc0230c993e988d3a8f7efd
Nyulian122 <103065477+Nyulian122@users.noreply.github.com>
2024-06-04 17:58:09 -0700

    Add files via upload

---
+++ js/data_todo.js
@@ -0,0 +1,15 @@
+let tareas = [{
+        estado: true,
+        id: 1,
+        texto: "Quitar todos los tornillos de la tap",
+    },
+    {
+        estado: false,
+        id: 2,
+        texto: "levantar la tapa con cuidado",
+    },
+]
+
+export {
+    tareas
+};
--- js/main.js
+++ js/main.js
@@ -1,27 +1,103 @@
-import { tareas } from './todo_.js';
+import {
+    tareas
+} from './data_todo.js';
 
 function cargar_tareas() {
-    let cuadro_de_tareas = document.querySelector(".root");
 
-    tareas.forEach(cada_tarea => {
+    let cuadro_de_tareas = document.querySelector(".lista_tareas");
+
+    tareas.forEach((cada_tarea) => {
+
         let div_tarea = document.createElement("div");
         div_tarea.classList.add("div_tareas");
 
-        div_tarea.innerHTML = `
-            <p class="text">${cada_tarea.texto}</p>
-            <div class="estado">${cada_tarea.estado ? '[X]' : '[_]'}</div>
+        if (cada_tarea.estado) {
+            div_tarea.innerHTML = `
+            <p class="texto">${cada_tarea.texto}</p>
+            <div class="estado">[/]</div>
         `;
-        
+        } else {
+            div_tarea.innerHTML = `
+            <p class="texto">${cada_tarea.texto}</p>
+            <div class="estado">[X]</div>
+        `;
+        }
         cuadro_de_tareas.appendChild(div_tarea);
     });
 }
-
 cargar_tareas();
 
 function cargar_botones() {
-    let caja_btn = document.querySelector(".botones");
-    
-    caja_btn.innerHTML = `<div class="btn_mas"> + </div>`;
+    let caja_btn = document.querySelector(".btn");
+
+    caja_btn.innerHTML = ` <div class="btn_mas"> + </div>`;
+}
+cargar_botones();
+
+function cargar_formulario() {
+    let ventana_formulario = document.querySelector(".formulario");
+    ventana_formulario.classList.add("activar_b")
+    ventana_formulario.innerHTML = `
+        <div class="div_controles">
+            <div class="btn_cerrar">X</div>
+        </div>
+
+        <div class="div_formulario">
+            <h2>Crear Tarea</h2>
+            <input type="txt" class="entrada-tarea">
+            <select id="categoria" name="categoria">
+                <option>Categoría</option>
+                <option>Casa</option>
+                <option>Escuela</option>
+                <option>Trabajo</option>
+            </select>
+        </div>
+
+        <div class="btn-crear">Crear</div>
+    `;
+
+    let btn_cerrar2 = document.querySelector(".btn_cerrar");
+    btn_cerrar2.addEventListener("click", () => {
+        ventana_formulario.classList.remove("activar_b")
+    });
+
+
+
+
+    let btn_crear = document.querySelector(".btn-crear");
+
+    btn_crear.addEventListener("click", () => {
+
+
+        let tarea = document.querySelector(".entrada-tarea").value;
+
+
+
+        let estructura_de_tarea = {
+            estado: true,
+            id: tarea,
+            texto: tarea,
+        };
+
+
+        tareas.push(estructura_de_tarea)
+
+
+        let cuadro_de_tareas = document.querySelector(".lista_tareas");
+        cuadro_de_tareas.innerHTML = "";
+
+
+        cargar_tareas();
+
+
+        ventana_formulario.classList.remove("activar_b")
+
+
+
+
+
+    })
 }
 
-cargar_botones();
+let btn_formulario = document.querySelector(".btn_mas");
+btn_formulario.addEventListener("click", cargar_formulario)
